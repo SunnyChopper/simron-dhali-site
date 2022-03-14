f12c107d3d8f14a50e5bf11e8b6b58e60b24d9b4
SunnyChopper <sunny@takecommandhealth.com>
2022-03-13 22:36:51 -0500

    Created RSVP modals for each event and properly linked them up to Google backend

--- scripts/main.js
+++ scripts/main.js
@@ -21,19 +21,114 @@ $('#cidercade-form').on('submit', function (e) {
       data: $(this).serialize(),
       success: function (response) {
         if (response.result == 'success') {
-          $('#cidercadeRsvpModal').modal('hide');
+          $('#cidercadeRsvpModal').hide();
+          $('.modal-backdrop').hide();
         } else {
           alert('Something went wrong. Please try again.');
         }
       },
-      error: function () {
-        alert('Something went wrong. Please try again.');
+      error: function (xhr, status, error) {
+        alert(xhr.responseText);
       }
     });
   } else {
     alert('Please fill out the form');
   }
 });
+
+$('#fogo-form').on('submit', function (e) {
+  e.preventDefault();
+
+  var formData = new FormData(this);
+
+  let name = formData.get('name');
+  let email = formData.get('email');
+
+  if (name.length > 0 && email.length > 0) {
+    $.ajax({
+      url: 'https://script.google.com/macros/s/AKfycbzfS7-40uhrEAfq5OaCXW6oruB7MAJkrtnfg079snFxbJQCxTM/exec',
+      method: 'POST',
+      dataType: 'json',
+      data: $(this).serialize(),
+      success: function (response) {
+        if (response.result == 'success') {
+          $('#fogoRsvpModal').hide();
+          $('.modal-backdrop').hide();
+        } else {
+          alert('Something went wrong. Please try again.');
+        }
+      },
+      error: function (xhr, status, error) {
+        alert(xhr.responseText);
+      }
+    });
+  } else {
+    alert('Please fill out the form');
+  }
+});
+
+$('#sippho-form').on('submit', function (e) {
+  e.preventDefault();
+
+  var formData = new FormData(this);
+
+  let name = formData.get('name');
+  let email = formData.get('email');
+
+  if (name.length > 0 && email.length > 0) {
+    $.ajax({
+      url: 'https://script.google.com/macros/s/AKfycbzfS7-40uhrEAfq5OaCXW6oruB7MAJkrtnfg079snFxbJQCxTM/exec',
+      method: 'POST',
+      dataType: 'json',
+      data: $(this).serialize(),
+      success: function (response) {
+        if (response.result == 'success') {
+          $('#sipPhoRsvpModal').hide();
+          $('.modal-backdrop').hide();
+        } else {
+          alert('Something went wrong. Please try again.');
+        }
+      },
+      error: function (xhr, status, error) {
+        alert(xhr.responseText);
+      }
+    });
+  } else {
+    alert('Please fill out the form');
+  }
+});
+
+$('#birthday-pregame-form').on('submit', function (e) {
+  e.preventDefault();
+
+  var formData = new FormData(this);
+
+  let name = formData.get('name');
+  let email = formData.get('email');
+
+  if (name.length > 0 && email.length > 0) {
+    $.ajax({
+      url: 'https://script.google.com/macros/s/AKfycbzfS7-40uhrEAfq5OaCXW6oruB7MAJkrtnfg079snFxbJQCxTM/exec',
+      method: 'POST',
+      dataType: 'json',
+      data: $(this).serialize(),
+      success: function (response) {
+        if (response.result == 'success') {
+          $('#birthdayPregameModal').hide();
+          $('.modal-backdrop').hide();
+        } else {
+          alert('Something went wrong. Please try again.');
+        }
+      },
+      error: function (xhr, status, error) {
+        alert(xhr.responseText);
+      }
+    });
+  } else {
+    alert('Please fill out the form');
+  }
+});
+
 function submitRsvp(e, formId) {
   e.preventDefault();
 
