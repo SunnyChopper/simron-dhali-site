2beace380d6f1816ff3254ad9b3b49a2c4fbb28c
SunnyChopper <sunny@takecommandhealth.com>
2022-03-13 20:13:32 -0500

    Added modal, links and further polishes

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,49 @@ AOS.init({
 });
 
 // Add your javascript here
+$('#cidercade-form').on('submit', function (e) {
+  e.preventDefault();
+
+  var formData = new FormData(this);
+
+  let name = formData.get('name');
+  let email = formData.get('email');
+
+  if (name.length > 0 && email.length > 0) {
+    $.ajax({
+      url: 'https://script.google.com/macros/s/AKfycbzfS7-40uhrEAfq5OaCXW6oruB7MAJkrtnfg079snFxbJQCxTM/exec',
+      method: 'POST',
+      dataType: 'json',
+      data: $(this).serialize(),
+      success: function (response) {
+        if (response.result == 'success') {
+          $('#cidercadeRsvpModal').modal('hide');
+        } else {
+          alert('Something went wrong. Please try again.');
+        }
+      },
+      error: function () {
+        alert('Something went wrong. Please try again.');
+      }
+    });
+  } else {
+    alert('Please fill out the form');
+  }
+});
+function submitRsvp(e, formId) {
+  e.preventDefault();
+
+  var formData = new FormData(document.querySelector(`#${formId}`));
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+
+  if (name.length > 0 && email.length > 0) {
+    let queryString = new URLSearchParams(formData).toString();
+
+    const xhr = new XMLHttpRequest();
+  } else {
+    alert('Please fill out the form');
+  }
+
+  return false;
+}
